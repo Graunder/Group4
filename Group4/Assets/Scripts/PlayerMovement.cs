ddcd7daa21e3f5228f67a9f6525d219b79889caa
Graunder <graunder@gmail.com>
2020-11-08 01:00:41 +0200

    test project for android

---
+++ Group4/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    public float speed = 5f;
+    public Joystick joystick;
+
+    private float moveHorizontal = 0f;
+    private bool jump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveHorizontal = joystick.Horizontal * speed;
+    }
+
+    public void Jump() {
+        jump = true;
+    }
+
+    private void FixedUpdate() {
+        controller.Move(moveHorizontal * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
