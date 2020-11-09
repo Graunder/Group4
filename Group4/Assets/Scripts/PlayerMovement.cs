5785cf7487c6791fd5828e075c1e12e2b82e5752
Graunder <graunder@gmail.com>
2020-11-10 01:56:55 +0200

    Update
    Added fall force
    Added a coin, coin animation, coin collider for collection

--- Group4/Assets/Scripts/PlayerMovement.cs
+++ Group4/Assets/Scripts/PlayerMovement.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
+    [SerializeField] private float fallForce = 400f;
+
     public CharacterController2D controller;
     public float speed = 5f;
     public Joystick joystick;
 
     private float moveHorizontal = 0f;
     private bool jump;
+    private Rigidbody2D playerRigidbody2D;
 
     // Start is called before the first frame update
     void Start()
@@ -27,12 +30,20 @@ public class PlayerMovement : MonoBehaviour
         moveHorizontal = Input.GetAxisRaw("Horizontal") * speed;
     }
 
+    public void Awake() {
+        playerRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
     public void Jump() {
         jump = true;
     }
 
     private void FixedUpdate() {
-        controller.Move(moveHorizontal * Time.fixedDeltaTime, false, jump);
+        controller.Move(moveHorizontal * Time.deltaTime, false, jump);
         jump = false;
+
+        if (playerRigidbody2D.velocity.y < -0.1) {
+            playerRigidbody2D.AddForce(new Vector2(0f, -fallForce));
+        }
     }
 }
