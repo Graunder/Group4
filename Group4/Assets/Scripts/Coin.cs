ad6153fb9e48339cb6b4ad679385a39d8f8064ec
Graunder <graunder@gmail.com>
2020-11-10 04:21:05 +0200

    Update
    Coins counting
    Changed some scaling

--- Group4/Assets/Scripts/Coin.cs
+++ Group4/Assets/Scripts/Coin.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
 
+    //public TextMeshProUGUI scoreText;
+    //public int score = 0;
+
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.tag == "Player") {
             Destroy(gameObject);
+            //score++;
+            //scoreText.text = score.ToString();
+            ScoreCounter.score++;
         }
     }
 
